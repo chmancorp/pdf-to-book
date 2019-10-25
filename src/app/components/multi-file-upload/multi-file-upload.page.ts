11c76542470d9f218798399278ef315955d95197
rrortegaa <morris400jul@gmail.com>
2019-10-25 15:30:22 -0500

    install jszip

--- src/app/components/multi-file-upload/multi-file-upload.page.ts
+++ src/app/components/multi-file-upload/multi-file-upload.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-var JSZip = require("jszip");
+import { saveAs } from 'file-saver';
+
+let JSZip = require("jszip");
 
 @Component({
   selector: 'app-multi-file-upload',
@@ -12,34 +14,74 @@ export class MultiFileUploadPage implements OnInit {
   dataZip: any;
   url;
   zipUrl: any;
+  zip = new JSZip();
+  zipBase: any;
+
   constructor() { }
   ngOnInit() {
     this.zipUrl = "";
   }
 
-  transformPDF(event) {
+  selectZip(data) {
     debugger;
-    var zip = new JSZip();
-    console.log(this.dataZip);
-    zip.loadAsync(event.target.files[0]).then(function(zip) {
-      // Leemos del archivo zip.
-      zip.file("Hola.txt").async("string"); // una promesa de "Hola Mundo\n"
-  });
-  //   JSZip.getBinaryContent(this.dataZip, function(err, data) {
-  //     if(err) {
-  //         throw err; // or handle err
-  //     }
-  
-  //     JSZip.loadAsync(data).then(function () {
-  //         // ...
-  //     });
-  // });
+    console.log(this.zip);
+    console.log(this.zipBase);
+
+  }
+  async transformPDF(event) {
+    debugger;
     var reader = new FileReader();
-      reader.readAsDataURL(event.target.files[0]); 
-      reader.onload = (imageRender: any) => { 
-        this.dataZip = imageRender.target.result;
+    var zip2 = new JSZip();
+    let base644;
+    console.log(event.target.files[0]);
+     let data1 = await this.zip.loadAsync(event.target.files[0]).then(function (zip) {
+        
+      // Leemos del archivo zip.
+      //  let zipBase = zip.generate({type:"base64"});
+      //  zip.file("Hola.txt").async("string"); // una promesa de "Hola Mundo\n"
+      var promise2 = zip.generateAsync({ type: "base64" }).then(async function (abc) {
+        console.log(abc)
+        
+        
+        base644 = await abc;
       //  console.log(this.dataZip);
-      }
+         saveAs(abc,"test.zip");
+        let promise3 = zip.generateAsync({ type: "blob" }).then(function (asd) {
+          saveAs(asd, "test2.zip");
+        
+        });
+      });
+debugger
+
+
+      // let promise = zip.generateAsync({ type: "blob" }).then(function (content) {
+      //   // see FileSaver.js
+      //   // saveAs(content, "example.zip");
+      //   reader.readAsDataURL(content);
+      //   reader.onload = (imageRender: any) => {
+      //     console.log(imageRender.target.result);
+      //     this.dataZip = imageRender.target.result;
+      //     console.log(this.dataZip);
+      //   }
+      // });
+
+    });
+    this.zipBase = base644;
+    // this.zipBase = this.zip.generateAsync({type: "blob"});
+    //   JSZip.getBinaryContent(this.dataZip, function(err, data) {
+    //     if(err) {
+    //         throw err; // or handle err
+    //     }
+
+    //     JSZip.loadAsync(data).then(function () {
+    //         // ...
+    //     });
+    // });
+    // reader.readAsDataURL(event.target.files[0]);
+    // reader.onload = (imageRender: any) => {
+    //   this.dataZip = imageRender.target.result;
+    //   console.log(this.dataZip);
+    // }
     //   var f = event.target.files[0]; // FileList object
     //   var reader = new FileReader();
     //   // Closure to capture the file information.
@@ -51,7 +93,7 @@ export class MultiFileUploadPage implements OnInit {
     //       console.log("testo=> ", base64String);
     //     }
     //   })(f);
-      
+
     // var reader = new FileReader();
     // reader.readAsDataURL(this.dataZip); 
     // reader.onload = (imageRender: any) => { 
